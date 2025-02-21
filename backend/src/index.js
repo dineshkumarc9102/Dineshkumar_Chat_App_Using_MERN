5deff7bbb40f3d3e5c67550764bc9cc814877cc6
dinesh kumar <cdk421413@gmail.com>
2025-02-21 15:51:31 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,10 +28,10 @@ app.use("/api/messages", messageRoutes);
 
 // for deploy
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
     app.get("*", (req, res ) => {
-        res.sendFile(path.join(__dirname,"../../frontend", "dist", "index.html"))
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
     })
 }
 
